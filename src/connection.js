ba616d175235c1ee2d69a8871f9a6d42d459fc5e
Davidmapa01 <davidmarpal@gmail.com>
2026-06-08 21:17:10 +0200

    MySQL connection update

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host:     process.env.DB_HOST     || 'sql7.freesqldatabase.com',
-    user:     process.env.DB_USER     || 'sql7824067',
-    password: process.env.DB_PASSWORD || 'dgDRtLqi5r',
-    database: process.env.DB_NAME     || 'sql7824067',
-    port:     process.env.DB_PORT     || 3306
+    host:     process.env.MYSQLHOST,
+    user:     process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port:     process.env.MYSQLPORT || 3306
 });
 
 db.connect((error) => {
