c2eb815892f5bf9b6b13606121f710a689f14924
Davidmapa01 <davidmarpal@gmail.com>
2026-06-08 21:50:24 +0200

    database name connection corrected

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host:     process.env.MYSQLHOST,
     user:     process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE,
     port:     process.env.MYSQLPORT || 3306
 });
 
